db1815ace869ab1513865728b2539cec27508df6
Ramphys Tirana Brito <158323887+user17job2@users.noreply.github.com>
2024-03-03 19:30:45 -0400

    creo finalizada la app de tareas

--- appDeTareas/functions.js
+++ appDeTareas/functions.js
@@ -1,8 +1,6 @@
-
   // Obtener la lista de tareas desde el localStorage
   const tareasGuardadas = JSON.parse(localStorage.getItem('tareas')) || [];
-  const descricionesGuardadas = JSON.parse(localStorage.getItem('descriciones')) || [];
-
+  // const descricionesGuardadas = JSON.parse(localStorage.getItem('descriciones')) || [];
   const btn = document.querySelector('.btnbtn');
   btn.addEventListener('click', agregarTarea);
   // btn.addEventListener('click', agregarDescripcion);
@@ -16,12 +14,11 @@ function mostrarTareas() {
           li.innerHTML = `<h3 class=h33>${tarea}</h3>`
 //                  por site llega a dificultar aqui va un textContent
           li.classList.add('tArea');
-          
+          li.setAttribute("name", "tareas")
          li.addEventListener('dblclick', () => eliminarTarea(index));
           listaTareas.appendChild(li);
       });
 }
-
   // Agregar una nueva tarea
 function agregarTarea(){
       const nuevaTarea = document.getElementById('nuevaTarea').value;
@@ -32,31 +29,18 @@ function agregarTarea(){
         document.getElementById('nuevaTarea').value = '';
       }
 }
-
   // Eliminar una tarea
-  function eliminarTarea(index) {
+function eliminarTarea(index) {
       tareasGuardadas.splice(index, 1);
       localStorage.setItem('tareas', JSON.stringify(tareasGuardadas));
       mostrarTareas();
-  }
+}
   // Mostrar las tareas al cargar la página
   mostrarTareas();
-// ////////
-//ahora aqui tratare de desarrollar los comentarios 
-
-let textArea = document.getElementById("listaTareas");
-
-// como me refiro al elemento que toque en una funcion
-// como utilizar el click y el dblclick en un solo elemento
-//  como verifico con un 'if' si un elemento existe en el dom
- //la condiicional para que no cree descriciones infinitas  
-//  ahora debo hacer que las descripciones de las tareas se guarden junto con que los div que contienen las tareas se hagan de un tamano fijo
 
 const inputT = document.querySelector('.inputT');  
 const addTitle = document.querySelector('.btn-addTitle');
-
 valueishon();
-
 addTitle.addEventListener('click', valueishon);
 function valueishon(){
 ver();      
@@ -85,18 +69,76 @@ function mostrar(){
 }
 
 // // Division
-//   const descripcionesGuardadas=JSON.parse(localStorage.getItem('descripciones')) || [];
-//   const btnt = document.getElementsByClassName("tArea");
+// Cambio de colores
+const Color =  document.getElementById('btnClasses');
+ let estado = true; 
+ Color.addEventListener('click', ()=>{
+if(estado=== true){
+    //nav
+    document.querySelector('nav').style.backgroundColor='rgb(25, 25, 50)';
+    document.querySelector('nav').style.border='1px solid white';
+    document.querySelector('nav').style.transition='1s';
+    //body
+    document.querySelector('body').style.backgroundColor='rgb(25, 25, 50)';
+    document.querySelector('body').style.transition='1s';
+    // inputs
+    document.querySelector('input').style.backgroundColor='rgb(25, 25, 68)'
+    document.querySelector('input').style.transition='1s'
+    //
+    document.querySelector('#nuevaTarea').style.backgroundColor='rgb(25, 25, 68)';
+    document.querySelector('#nuevaTarea').style.transition='1s'
+    //sus botones 
+    document.querySelector('.btn-addTitle').style.backgroundColor='rgb(25, 25, 68';
+    document.querySelector('.btn-addTitle').style.transition='1s';
+    //
+    document.querySelector('.btnbtn').style.backgroundColor='rgb(25, 25, 68';
+    document.querySelector('.btnbtn').style.transition='1s';
+    //seccion contenedora
+    document.querySelector('section').style.backgroundColor='rgb(25, 35, 70)';
+    document.querySelector('section').style.border='1px solid white';
 
+    document.querySelector('section').style.transition='1s';
+    //tareas
+    // document.getElementsByName('tarea').style.backgroundColor='red';
+    // console.log(tareasGuardadas)
+    const elementosMenu = document.getElementsByClassName('tArea');
+    for (let i = 0; i < elementosMenu.length; i++) {
+    elementosMenu[i].style.backgroundColor = 'transparent';
+    elementosMenu[i].style.transition = '1s';
+    }
+    estado=false;
+}else{
+    document.querySelector('nav').style.transition='1s';
+    document.querySelector('nav').style.border='none';
+    document.querySelector('nav').style.backgroundColor='transparent';
+    
+    document.querySelector('body').style.backgroundColor='rgb(12, 99, 125)';
+    document.querySelector('body').style.transition='1s';
 
+    document.querySelector('input').style.backgroundColor='rgb(15, 100, 125)'
+    document.querySelector('input').style.transition='1s'
 
-let textaT = document.querySelectorAll('.textaT');
+    document.querySelector('#nuevaTarea').style.backgroundColor='rgb(15, 100, 125)';
+    document.querySelector('#nuevaTarea').style.transition='1s'
 
-// textaT.addEventListener("blur", () => {
-//   // Aquí puedes realizar acciones cuando el textarea obtiene el foco
-//   console.log("El textarea ha recibido el foco.");
-//   mostrarTareas();
-// });
+    document.querySelector('.btn-addTitle').style.backgroundColor='rgb(12, 99, 125)';
+    document.querySelector('.btn-addTitle').style.transition='1s';
+
+    document.querySelector('.btnbtn').style.backgroundColor='rgb(12, 99, 125)';
+    document.querySelector('.btnbtn').style.transition='1s';
+
+    document.querySelector('section').style.backgroundColor=' rgb(27, 79, 101)';
+    document.querySelector('section').style.transition='1s';
+
+    const elementosMenu = document.getElementsByClassName('tArea');
+    for (let i = 0; i < elementosMenu.length; i++) {
+    elementosMenu[i].style.backgroundColor ='antiquewhite';
+    elementosMenu[i].style.transition ='1s';  
+    }
+    estado=true
+  }
+
+});
 
 
 //  zona de codigo reusable
